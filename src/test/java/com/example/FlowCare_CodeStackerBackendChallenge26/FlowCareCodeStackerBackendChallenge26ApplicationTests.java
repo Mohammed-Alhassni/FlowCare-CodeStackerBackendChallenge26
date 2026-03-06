ac972ff726524adb0ce8ae2692a27d7fc4881a1e
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-03-06 19:17:25 +0400

    Initial commit

---
+++ src/test/java/com/example/FlowCare_CodeStackerBackendChallenge26/FlowCareCodeStackerBackendChallenge26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlowCare_CodeStackerBackendChallenge26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowCareCodeStackerBackendChallenge26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
