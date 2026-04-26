bea5d2df2f73b68d3ebc1b7f084dfd0df0c5b445
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-04-26 10:01:15 +0400

    Updated folders and main app naming

---
+++ src/main/java/FlowCareBackend/FlowCareBackend.java
@@ -0,0 +1,13 @@
+package FlowCareBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowCareBackend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowCareBackend.class, args);
+	}
+
+}
--- src/test/java/FlowCareBackend/FlowCareCodeStackerBackendChallenge26ApplicationTests.java
+++ src/test/java/FlowCareBackend/FlowCareCodeStackerBackendChallenge26ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.FlowCare_CodeStackerBackendChallenge26;
+package FlowCareBackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
